552dd1fe20a5041379bfc271a52452d06aa9dfad
Muhsin Abdul-Musawwir <mabdulmusawwir@esurance.com>
2017-11-16 13:37:23 -0800

    Got react working

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
 import { hello } from './index2.js';
-console.log(hello);
-document.write(hello);
+
+const app = document.getElementById('app');
+
+const App = () => (
+  <div>
+    <h1>{hello}</h1>
+  </div>
+);
+
+render(<App />, app);
+
+// console.log(hello);
+// document.write(hello);
--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,12 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: 'dist/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, use: {
+        loader: 'babel-loader'
+      }, exclude: /node_modules/ }
+    ]
   }
 }
