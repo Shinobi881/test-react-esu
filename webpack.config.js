675576b05e6b78175470592b526281b5e695cbce
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2017-11-16 20:52:16 -0800

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: 'dist',
+    path: `${__dirname}/dist`,
     filename: 'bundle.js'
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, use: {
-        loader: 'babel-loader'
-      }, exclude: /node_modules/ }
+      { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ }
     ]
   }
 }
